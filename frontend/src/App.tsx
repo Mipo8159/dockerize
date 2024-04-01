ea678b33abf403f1ec22ee3c5d0b1595ffadc412
Giga Kvachakhia <stalkerglac@gmail.com>
2024-04-02 02:14:37 +0400

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import AppRouter from "./router/AppRouter";
+
+function App() {
+  return (
+    <>
+      <h1>Hello there</h1>
+      <AppRouter />
+    </>
+  );
+}
+
+export default App;
